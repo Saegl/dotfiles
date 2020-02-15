abb1570e7bbdbafac4045830a298ada9222341ab
Saegl <saegl@protonmail.com>
2020-02-15 21:20:36 +0600

    add template for C++

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 My dotfiles for vim and bash
+
+Short description of each file:
+temp.cpp - My current C++ template for codeforces problems
+oldtemp.cpp - template lines that could be useful, but I do not use them now
+
---
+++ temp.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define N "\n"
+
+template<typename T>
+ostream &operator<<(ostream &os, vector<T> const &v) {
+        os << "<vector> ";
+        for (T x : v) {
+                os << x << " ";
+        }
+        return os;
+}
+
+#define dbg(name) ([&name]() { cerr << #name": " << name << endl; return name;})()
+#define times(t) int t; cin >> t; for(int i = 0; i < t; i++)
+#define read(n) int n; cin >> n;
+#define readll(n) ll n; cin >> n;
+#define reads(n) string n; cin >> n;
+
+void solution(void);
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    solution();
+}
+
+void solution() {
+}
+
